5e781fc77ea110c737fbf5a8d7b56aba714dadc1
Ash <3902748+TheAshGits@users.noreply.github.com>
2018-07-16 13:45:48 -0500

    ng build

---
+++ classtest/src/app/shared/header/header.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  isCollapsed = true;
+  loggedInUser = this.authService.getUser();
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  logout() {
+    this.authService.logout().subscribe(() => {
+      this.router.navigate(['/login']);
+    });
+  }
+
+  toggleMenu() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+}
---
+++ classtest/src/app/shared/services/todo.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Todo } from '../classes/todo';
+import { environment } from '../../../environments/environment';
+
+const requestOptions = { withCredentials: true };
+
+
+@Injectable()
+export class TodoService {
+  constructor(private http: HttpClient) {}
+
+  private url = `${environment.apiBaseUrl}/todo`;
+
+  save(item: string): Observable<Todo> {
+    return this.http.post<Todo>(
+      this.url,
+      new Todo(item),
+      requestOptions
+    );
+  }
+
+  getAll(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.url, requestOptions);
+  }
+
+  updateTodo(todo: Todo): Observable<string> {
+    const url =  `${this.url}/${todo.id}`;
+
+    return this.http.patch(url, todo, {
+      withCredentials: true,
+      responseType: 'text',
+    });
+  }
+
+  deleteTodo(todo: Todo): Observable<string> {
+    const url =  `${this.url}/${todo.id}`;
+
+    return this.http.delete(url, {
+      withCredentials: true,
+      responseType: 'text',
+    });
+  }
+}
