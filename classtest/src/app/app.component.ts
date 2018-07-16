5c0c4fcae11c51a74f9cb7125c02dff0f55acf40
Ash <3902748+TheAshGits@users.noreply.github.com>
2018-07-16 11:12:10 -0500

    added classtest

---
+++ classtest/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+    title = 'Our Awesome Todo App';
+}
